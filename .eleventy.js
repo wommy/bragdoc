8dae83b0e90f787e6526ef0ce38ae4e253c72be0
wommy <tom@technomad.media>
2021-12-31 11:39:17 -0500

    init: tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
-module.exports = {
-	dir: {
-		input: 'src',
-		output: 'dist',
-		includes: 'components',
+module.exports = function(c) {
+	c.addWatchTarget('./src/style/')
+	return {
+		dir: {
+			input: 'src',
+			output: 'dist',
+			includes: 'components',
+		}
 	}
 }
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./src/**/*.njk"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
