427c50dbc6745034f27495c0d639623e5b2df753
wommy <tom@technomad.media>
2022-01-01 16:23:53 -0500

    wip: luxon for date

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,11 @@ module.exports = function(c) {
 	c.addWatchTarget('./src/style/')
 	c.addFilter('displayDate', require('./src/filters/displayDate.js'))
 
+		// human readable date
+	c.addFilter("readableDate", dateObj => require('luxon')
+		.DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("dd LLL yyyy")
+	)
+
 	return {
 		dir: {
 			input: 'src',
